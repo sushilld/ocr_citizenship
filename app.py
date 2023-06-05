f33257fa98c217640a4dc58cee0f411f9ac8fd63
Sushil Dahakal <boyc50714@gmail.com>
2023-06-05 12:38:41 +0545

    added file upload function

--- app.py
+++ app.py
@@ -9,13 +9,26 @@ def home_tab():
     if st.button("Submit"):
         if uploaded_front is not None and uploaded_back is not None:
             # Process the uploaded images here
+            st.session_state['uploaded_front'] = uploaded_front
+            st.session_state['uploaded_back'] = uploaded_back
             st.write("Front and back of citizenship uploaded successfully!")
             st.warning("Please switch to other tabs.")
         else:
             st.error("Please upload both the citizenship front and back.")
 
 def tab2(uploaded_front):
-    st.write("This is the second tab.")
+    st.header("Uploaded Front OCR")
+    col1, col2 = st.columns([1, 3])
+
+    # Add the image to the first column
+    with col1:
+        st.subheader("Citizenship Front View")
+        st.image(uploaded_front, use_column_width=True)
+
+    # Add the description to the second column
+    with col2:
+        st.subheader("Citizenship OCR finding")
+        st.write("This is a description of the image.")
 
 def tab3(uploaded_back):
     st.write("This is the third tab.")
@@ -36,6 +49,7 @@ def main():
         home_tab()
     elif selected_tab == "Front OCR":
         uploaded_front = st.session_state.get("uploaded_front")
+        print(uploaded_front)
         if uploaded_front is None :
             st.error("Please upload both the citizenship front and back.")
         else:
